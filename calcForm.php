569a818d263cdb82af907869c2ec9d4152fd0263
juniordlt <58046748+juniordlt@users.noreply.github.com>
2019-11-21 11:28:50 -0800

    Create calcForm.php

---
+++ calcForm.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+	<style>
+		#peeps{
+		color:red;
+		}
+	
+	</style>
+	
+	
+	
+	<body>
+	<form action="runCalc.php" method="'get">
+		Num1 <input type="text" name="num1"><br>
+		Num2 <input type="text" name="num2"><br>
+	<select name="type">
+	<option>add</option>
+	<option>subtract</option>
+	</select>
+	<input type="submit" value="Calculate">
+</form>	
+	
+	</body>
+	</head>
+
+
+
+
+
+</html>
