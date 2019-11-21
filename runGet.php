b477f1f65eb29bddf7704dd95235303de333ab8d
juniordlt <58046748+juniordlt@users.noreply.github.com>
2019-11-21 11:28:12 -0800

    Create runGet.php

---
+++ runGet.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		<form action="runPost.php" method="post">
+			Name: <input type="text" name="name"><br>
+			E-mail: <input type="text" name="email"><br>
+			<input type="submit" value="Post">
+		</form>
+		<form action="runGet.php" method="get">
+		Name: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		<input type="submit" value="Get">
+		</form>
+	</body>
+</html>
