d1bb0e657619fabbecd2ad849e4889db16d430cb
juniordlt <58046748+juniordlt@users.noreply.github.com>
2019-11-21 10:15:46 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+<html>
+	<body>
+		<head>
+		<style>
+		#peeps {
+			color:blue;
+			]
+		</style>
+	<?php 
+		echo "hello world";
+		echo "<h1 id='peeps'>hello people</h1>";
+		
+		?>
+
+
+		</head>
+
+	</body>  
+
+</html>
