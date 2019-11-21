5b2665d7490105213ec90b94639706360805a6ea
juniordlt <58046748+juniordlt@users.noreply.github.com>
2019-11-21 11:27:33 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<style>
+			#bannerDiv{
+				background-color:green;
+				height:90px;
+				width:600px;
+			}
+			.links {
+				color:black;
+				font-size:60px
+			}
+			
+			
+			
+		</style>
+	</head>
+	<body> 
+		
+		
+		
+		<div id="bannerdiv">
+			<a class="links" href="test.php">test page</a> 
+			<a class="links" href="form.php">form page</a> 
+			<a class="links" href="calcForm.php">form page</a>
+			
+
+		</div>
+	</body>
+
+</html>
