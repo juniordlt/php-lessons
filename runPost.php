f3f109d39a72800e043fc886add1cb4e378f6d2a
juniordlt <58046748+juniordlt@users.noreply.github.com>
2019-11-21 11:28:29 -0800

    Create runPost.php

---
+++ runPost.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		<?php
+			$name = $_POST["name"];
+			$email = $_POST["email"];
+			echo "Your name is ".$name." and your email is ".$email;
+		?>
+	</body>
+</html>
